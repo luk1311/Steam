6f2651ce693fdb352a4d5e5a3df198f49819682d
luka <luka.bp1311@gmail.com>
2026-02-03 19:27:19 -0500

    cambios

---
+++ trabajo 2/script.js
@@ -0,0 +1,30 @@
+let temp= prompt("Ingrese la temperatura en grados Celsius:");
+
+function temperatura(temp){
+    if(isNaN(temp)){
+        console.log("El valor ingresado no es un número válido.");
+        alert("Por favor, ingrese un número válido para la temperatura.");
+        return;
+    }       
+        if(temp<=0){
+            console.log(`La temperatura es de ${temp} grados Celsius, por lo tanto es FRIO.`);
+            alert("FRIO");
+        }
+        else if (temp>0 && temp<=15){
+            console.log(`La temperatura es de ${temp} grados Celsius, por lo tanto es TEMPLADO.`);
+        }
+        else if (temp>15 && temp<=25){
+            console.log(`La temperatura es de ${temp} grados Celsius, por lo tanto es AGRADABLE.`);
+        }
+        else if (temp>25 && temp<=35){
+            console.log(`La temperatura es de ${temp} grados Celsius, por lo tanto es CALUROSO.`);
+            alert("CALUROSO");
+        }
+        else{
+            console.log(`La temperatura es de ${temp} grados Celsius, por lo tanto es MUY CALUROSO / SOFOCANTE.`);
+            alert("MUY CALUROSO!!!!!");
+        }
+}
+
+
+temperatura(temp);

fae010a3aea0e60f3c31ba3a054569d6c2f059dc
luka <luka.bp1311@gmail.com>
2026-02-11 18:23:35 -0500

    cambios

--- formulario/script.js
+++ formulario/script.js
@@ -6,8 +6,6 @@ function acceso(){
         alert(`Ingresa todos los campos obligatorios`);
     }else if(username.value != "Lukas" || rol.value != "Estudiante"){
         alert(`Acceso denegado`);
-    }else if(rol.value == "Profesor"){
-        alert(`Acceso denegado ${username.value}`);
     }
     else{
         alert(`Acceso concedido ${username.value}`);

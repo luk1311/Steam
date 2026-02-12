665472e3c55ac467cd045d39481ce1428d5f4ead
luka <luka.bp1311@gmail.com>
2026-02-12 18:08:21 -0500

    addcion de operaciones

--- calculadora/script.js
+++ calculadora/script.js
@@ -1,6 +1,6 @@
 
 
-function calcular() { 
+function sumar() { 
     const parrafo = document.getElementById("resultado");
     const num1 = document.getElementById("num1").value;
     const num2 = document.getElementById("num2").value;
@@ -11,3 +11,27 @@ function calcular() {
         alert("Por favor, introduce un numero.");
     }   
 }
+
+function restar() { 
+    const parrafo = document.getElementById("resultado");
+    const num1 = document.getElementById("num1").value;
+    const num2 = document.getElementById("num2").value;
+    const resultado = parseFloat(num1) - parseFloat(num2);
+    parrafo.innerText = "El resultado es: " + resultado;
+    
+    if (isNaN(resultado)) {
+        alert("Por favor, introduce un numero.");
+    }   
+}
+
+function multiplicar() { 
+    const parrafo = document.getElementById("resultado");
+    const num1 = document.getElementById("num1").value;
+    const num2 = document.getElementById("num2").value;
+    const resultado = parseFloat(num1) * parseFloat(num2);
+    parrafo.innerText = "El resultado es: " + resultado;
+    
+    if (isNaN(resultado)) {
+        alert("Por favor, introduce un numero.");
+    }   
+}   

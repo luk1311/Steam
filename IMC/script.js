6d4d2a78e0560ef290cbfc5919b9904306b01b2e
luka <luka.bp1311@gmail.com>
2026-02-18 18:35:15 -0500

    correciones

--- IMC/script.js
+++ IMC/script.js
@@ -13,27 +13,38 @@ function calcularIMC() {
     const imc = pesoValue / (alturaValue * alturaValue);
     const resultado = document.getElementById('resultado');
     
-    resultado.innerText = `Tu IMC es: ${imc.toFixed(2)}`;
+    
     const number = document.getElementById('number');
         if (imc <= 16) {
+            resultado.innerText = "Desnutrición severa";
             number.style.color = "red";
             number.innerText = imc.toFixed(1);
         } else if (imc <= 18.5) {
+            resultado.innerText = "Desnutrición moderada";
             number.style.color = "orange";
             number.innerText = imc.toFixed(1);
         } else if (imc <= 22) {
+            resultado.innerText = "Bajo peso";
             number.style.color = "yellow";
             number.innerText = imc.toFixed(1);
         } else if (imc <= 24.5) {
+            resultado.innerText = "Peso normal";
             number.style.color = "green";
             number.innerText = imc.toFixed(1);
+        } else if (imc <= 30) {
+            resultado.innerText = "Sobrepeso";
+            number.style.color = "orange";
+            number.innerText = imc.toFixed(1);
         } else if (imc <= 35) {
+            resultado.innerText = "Obesidad grado 1";
             number.style.color = "orange";
             number.innerText = imc.toFixed(1);
         } else if (imc <= 40) {
+            resultado.innerText = "Obesidad grado II";
             number.style.color = "lightred";
             number.innerText = imc.toFixed(1);
         } else {
+            resultado.innerText = "Obesidad grado III";
             number.style.color = "red";
             number.innerText = imc.toFixed(1);
         }

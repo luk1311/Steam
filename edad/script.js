b616a8c24ddc01a408c854b9ec6df70ec06510b8
luka <luka.bp1311@gmail.com>
2026-02-25 18:15:59 -0500

    finalizacion

--- edad/script.js
+++ edad/script.js
@@ -3,32 +3,38 @@ const resultado = document.getElementById("resultado");
 
 
 function calcular() {
+    //variables
     const nacimiento = new Date(fechaInput.value);
     const hoy = new Date();
+
+    // Operaciones
     let edad = hoy.getFullYear() - nacimiento.getFullYear();
     const mes = hoy.getMonth() - nacimiento.getMonth();
+    const dia = hoy.getDate() - nacimiento.getUTCDate();
 
     console.log(mes, hoy.getDate() <= nacimiento.getDate(), hoy.getDate(), nacimiento.getUTCDate());
 
-
-
+    // Validaciónes
     if (isNaN(nacimiento)) {
         alert("Por favor, ingresa una fecha válida.");
         return;
     } 
-    if (mes <= 0 || (mes === 0 && hoy.getDate() <= nacimiento.getDate())) {
+
+    if (mes < 0 || (mes === 0 && dia < 0)) {
         edad--;
     } 
+
     if (edad < 0) {
         resultado.innerText = "¡Aún no has nacido!";
-    
-    }else {
-        resultado.innerText = "Tu edad es: " + edad + " años";
+        return;
     }
    
+    // Felicitar en el día del cumpleaños
     if (mes === 0 && hoy.getDate() === nacimiento.getUTCDate()) {
         resultado.innerText = "¡Feliz cumpleaños! Hoy cumples " + edad + " años.";
         return;
-    } 
+    } else {
+        resultado.innerText = "Tienes " + edad + " años.";
+    }
    
 }

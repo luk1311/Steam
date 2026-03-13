3738b0f8bd546395e353ff5dd7edf7c0107011e2
luka <luka.bp1311@gmail.com>
2026-03-13 08:24:45 -0500

    adaptacion

--- calculadora/script.js
+++ calculadora/script.js
@@ -1,36 +1,66 @@
 
 
 function sumar() { 
+    //VARIABLES
     const parrafo = document.getElementById("resultado");
     const num1 = document.getElementById("num1").value;
     const num2 = document.getElementById("num2").value;
     const resultado = parseFloat(num1) + parseFloat(num2);
-    parrafo.innerText = "El resultado es: " + resultado;
+    const newparrafo = document.createElement("p");
+   
+
+    //HEREDAR ELEMENTO
+    const historial = document.querySelector("#historial");
+    historial.appendChild(newparrafo);
     
+    //EDITAR ELEMENTO
+     newparrafo.textContent = resultado;
+
+    //CONDIONAL
     if (isNaN(resultado)) {
         alert("Por favor, introduce un numero.");
     }   
 }
 
 function restar() { 
+    //VARIABLES
     const parrafo = document.getElementById("resultado");
     const num1 = document.getElementById("num1").value;
     const num2 = document.getElementById("num2").value;
     const resultado = parseFloat(num1) - parseFloat(num2);
-    parrafo.innerText = "El resultado es: " + resultado;
+    const newparrafo = document.createElement("p");
+   
+
+    //HEREDAR ELEMENTO
+    const historial = document.querySelector("#historial");
+    historial.appendChild(newparrafo);
     
+    //EDITAR ELEMENTO
+     newparrafo.textContent = resultado;
+
+    //CONDICIONAL
     if (isNaN(resultado)) {
         alert("Por favor, introduce un numero.");
     }   
 }
 
 function multiplicar() { 
+    //VARIABLES
     const parrafo = document.getElementById("resultado");
     const num1 = document.getElementById("num1").value;
     const num2 = document.getElementById("num2").value;
     const resultado = parseFloat(num1) * parseFloat(num2);
-    parrafo.innerText = "El resultado es: " + resultado;
+    const newparrafo = document.createElement("p");
+   
+
+    //HEREDAR ELEMENTO
+    const historial = document.querySelector("#historial");
+    historial.appendChild(newparrafo);
     
+    //EDITAR ELEMENTO
+     newparrafo.textContent = resultado;
+
+    //CONDICIONAL
     if (isNaN(resultado)) {
         alert("Por favor, introduce un numero.");
     }   
